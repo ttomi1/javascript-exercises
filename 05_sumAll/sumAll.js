c75cdb09c4378a3807ed0cadf16598d60875015c
ttomi1 <[200~139554402+ttomi1@users.noreply.github.com~>
2023-09-21 19:14:48 +0200

    Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(typeof a === "number" && typeof b === "number" && (a > 0) && (b > 0)){
+        if(a > b){
+            let temp = a;
+            a = b;
+            b = temp;
+        }
+        let sum = 0;
+        for(let i = a; i <= b; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else
+        return "ERROR";
 };
 
 // Do not edit below this line
