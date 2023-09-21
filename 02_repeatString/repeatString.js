1f83fba49d52a696fcdbab1f9714a68f3226d415
ttomi1 <[200~139554402+ttomi1@users.noreply.github.com~>
2023-09-21 16:42:51 +0200

    A

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let result = "";
+    if(num < 0) return "ERROR";
+    for(let i  = 0; i < num; i++){
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr = [], ...remove) {
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < remove.length; j++){
+            if(arr[i] === remove[j]){
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    } 
+    return arr;
 };
 
 // Do not edit below this line
