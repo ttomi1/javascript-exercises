f2825b7c1e44a2f5f6cf4ae849268a8dd36cc4cf
ttomi1 <tomi.zir@gmail.com>
2024-05-26 15:32:20 +0200

    ex

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a < 0) return "OOPS";
+    a = +a;
+    let countFirst = 0;
+    let countSecond = 1;
+    let result = 0;
+    for(let i = 0; i < a; i++){
+        result = result + countSecond;
+        countSecond = result;
+    }
 };
 
+// 0    1	1	2	3	
 // Do not edit below this line
 module.exports = fibonacci;
