c4fa38b4dbf463e5af405bc52f41adb432af6dce
ttomi1 <tomi.zir@gmail.com>
2024-05-26 13:35:33 +0200

    exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(...a) {
+	return Math.pow(a[0], a[1]);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	const b = [];
+  for (let i = 1; i <= a; i++){
+    b.push(i);
+  }
+  return b.reduce((sum, curr) => sum * curr, 1);
 };
+
+b = factorial(1);
 
 // Do not edit below this line
 module.exports = {
